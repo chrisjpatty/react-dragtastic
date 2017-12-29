7b238ba956f7e3f3ea396bf6d82e4c61a8770c40
Christopher Patty <christopher.jon.patty@gmail.com>
2017-12-28 18:18:50 -0700

    Added index.js on root for systems that don't respect package main

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.DragComponent = exports.Draggable = exports.Droppable = undefined;
+
+var _index = require('./build/index.js');
+
+var Droppable = exports.Droppable = _index.Droppable;
+var Draggable = exports.Draggable = _index.Draggable;
+var DragComponent = exports.DragComponent = _index.DragComponent;
